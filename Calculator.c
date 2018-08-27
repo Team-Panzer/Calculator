da1864e2d2c4e0c9314b1e948f22b9c28a5f450a
Zeocode <savdutt01@gmail.com>
2018-08-28 00:00:18 +0530

    1st version

---
+++ Calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	printf("Enter Two numbers\n");
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	int sum,prod,div,mod;
+	sum=a+b;
+	// Saayan do product here
+	// Shrivas do division here
+	// Sayantan do modulus or remainder here;
+	printf("SUM=%d",sum);
+}
+	
